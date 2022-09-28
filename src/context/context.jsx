c8c2eb5b2830e52567ff37c4aba9a3fa179511c8
themodernmonk7 <sauravavishek@gmail.com>
2022-09-28 09:34:43 +0530

    04-URLs setup

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect, useState } from "react"
 
+//** =================== URLs =================== */
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainURL = `https://api.unsplash.com/photos/`
+const searchURL = `https://api.unsplash.com/search/photos/`
+//** =========================================== */
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
