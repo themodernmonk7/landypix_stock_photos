f453c48f2396c3701d08d9a68c389fd0db86661f
themodernmonk7 <sauravavishek@gmail.com>
2022-09-28 10:28:36 +0530

    12-Fetched image wiht scroll

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -11,16 +11,20 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
 
   // Fetch Images
   const fetchImages = async () => {
     setLoading(true)
     let url
-    url = `${mainURL}${clientID}`
+    const urlPage = `&page=${page}`
+    url = `${mainURL}${clientID}${urlPage}`
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setPhotos(data)
+      setPhotos((oldPhotos) => {
+        return [...oldPhotos, ...data]
+      })
       setLoading(false)
     } catch (error) {
       console.log(error)
@@ -30,15 +34,17 @@ const AppProvider = ({ children }) => {
 
   useEffect(() => {
     fetchImages()
-  }, [])
+  }, [page])
 
   useEffect(() => {
     const event = window.addEventListener("scroll", () => {
       if (
         !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 10
       ) {
-        console.log("it worked")
+        setPage((oldPage) => {
+          return oldPage + 1
+        })
       }
     })
     return () => window.removeEventListener("scroll", event)
