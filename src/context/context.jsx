7d829ba81a4ae1e5e41a44a38ed8663d15d8b41d
themodernmonk7 <sauravavishek@gmail.com>
2022-09-28 09:29:43 +0530

    03-React context setup

---
+++ src/context/context.jsx
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect, useState } from "react"
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+}
+
+export { AppContext, AppProvider }
